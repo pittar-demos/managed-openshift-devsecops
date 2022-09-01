51ff9e903706020f276275a68b0d0ce26cf37b56
Andrew Pitt <apitt@redhat.com>
2022-09-01 14:55:13 -0400

    Updated pipelines

--- 00-setup/add-consolelinks.sh
+++ 00-setup/add-consolelinks.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+apps=$(oc get ingresscontroller default -n openshift-ingress-operator -o jsonpath='{.status.domain}')
+
 # Add DevSpace link.
-echo "  href: https://devspaces."$(oc get ingresscontroller default -n openshift-ingress-operator -o jsonpath='{.status.domain}')"#http://gitea.scm.svc.cluster.local:3000/developer/battle-rest-heroes/raw/branch/main/devfile.yaml" \
+echo "  href: https://devspaces.$apps#http://gitea.scm.svc.cluster.local:3000/developer/battle-rest-heroes/raw/branch/main/devfile.yaml" \
   >> console-links/battle-devspace.yaml
 
+# Add DevSpace link.
+echo "  href: https://gitea-scm.$apps" \
+  >> console-links/scm-gitea.yaml
+
 oc create -f console-links/battle-devspace.yaml
+oc create -f console-links/scm-gitea.yaml
