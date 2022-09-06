873c077f953099c390133f86249e1ffe119f8f33
Andrew Pitt <apitt@redhat.com>
2022-09-06 10:42:49 -0400

    Updated console links.

--- 00-setup/add-consolelinks.sh
+++ 00-setup/add-consolelinks.sh
@@ -10,5 +10,16 @@ echo "  href: https://devspaces.$apps#http://gitea.scm.svc.cluster.local:3000/de
 echo "  href: https://gitea-scm.$apps" \
   >> console-links/scm-gitea.yaml
 
+# Add Nexus link.
+echo "  href: https://nexus-cicd-tools.$apps" \
+  >> console-links/nexus.yaml
+
+# Add SonarQube link.
+echo "  href: https://sonarqube-cicd-tools.$apps" \
+  >> console-links/sonarqube.yaml
+
+
 oc create -f console-links/battle-devspace.yaml
 oc create -f console-links/scm-gitea.yaml
+oc create -f console-links/nexus.yaml
+oc create -f console-links/sonarqube.yaml
